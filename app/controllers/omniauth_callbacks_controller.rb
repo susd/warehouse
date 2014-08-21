e0d737efba299aa3baa5eb0bc643643f3098acae
Eric Hayes <eric@erichayes.net>
2014-08-21 11:16:14 -0700

    Base setup for OAuth testing

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,33 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  # skip_before_filter :authenticate_user!
+  def all
+    user = User.from_omniauth(auth_params)
+    if user.persisted?
+      flash[:notice] = "You are in..!!! Go to edit profile to see the status for the accounts"
+      sign_in_and_redirect(user)
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    #handle you logic here..
+    #and delegate to super.
+    binding.pry
+    super
+  end
+  
+  alias_method :google_oauth2, :all
+  alias_method :google, :all
+  
+  protected
+  
+  def auth_params
+    # auth = env["omniauth.auth"]
+    # auth.permit({provider: "", uid: "", info: [:name, :email, :first_name, :last_name, :image]})
+    ActionController::Parameters.new(request.env["omniauth.auth"]).permit!
+  end
+
+end
