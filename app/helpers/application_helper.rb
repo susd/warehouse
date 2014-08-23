b9449ea709bfbd6e51784b07216e5b34cae05a4b
Eric Hayes <eric@erichayes.net>
2014-08-22 21:30:59 -0700

    Bootstrap navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def active_li(options)
+    cls = "active" if current_page?(options)
+    content_tag :li, class: cls do
+      yield if block_given?
+    end
+  end
 end
