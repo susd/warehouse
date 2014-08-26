1b1bd08073828813a5ad6e75845154cd509a06ea
Eric Hayes <eric@erichayes.net>
2014-08-26 16:30:03 -0700

    Test driving ProductImporter

--- app/services/product_importer.rb
+++ app/services/product_importer.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-class ProductsImporter
+class ProductImporter
   attr_reader :data
   
   def initialize(path)
@@ -11,9 +11,21 @@ class ProductsImporter
     
   end
   
+  def page_header?(row)
+    row.include? "Page No"
+  end
+  
+  def group_header?(row)
+    !!row.detect{|c| c =~ /WH-\d+/ }
+  end
+  
+  def column_header?(row)
+    row.include? "Item  ID"
+  end
+  
   private
   
-  def import_flat(csv_data)
-    csv_data.map{ |a| a.compact }.keep_if{ |a| a.any? }
+  def import_flat(data)
+    data.map{ |a| a.compact }.keep_if{ |a| a.any? }
   end
 end
--- test/services/product_importer_test.rb
+++ test/services/product_importer_test.rb
@@ -3,11 +3,37 @@ require 'test_helper'
 class ProductImporterTest < ActiveSupport::TestCase
   
   setup do
-    @importer = ProductImporter.new(Rails.root + 'test/fixtures/catalog.csv')
+    @importer = ProductImporter.new('test/fixtures/catalog.csv')
   end
   
   test "Reading a CSV file" do
     assert_equal ["SAUGUS UNION SCHOOL DISTRICT", "Page No", "1"], @importer.data.first
+  end
+  
+  test "Finding a page header" do
+    assert @importer.page_header? ["SAUGUS UNION SCHOOL DISTRICT", "Page No", "1"]
+  end
+  
+  test "Finding a group header" do
+    assert @importer.group_header? ["WH-05", " - ", "CUSTODIAL SUPPLIES"]
+    refute @importer.group_header? ["SAUGUS UNION SCHOOL DISTRICT", "Page No", "1"]
+    refute @importer.group_header? ["05-00827", "NIGHT LIGHT 13W", "EA", "2.60000"]
+  end
+  
+  test "Finding a column header" do
+    assert @importer.column_header? ["Item  ID", "Item Description", "UOM", "Unit Cost"]
+  end
+  
+  test "Finding a pre-existing product" do
+    skip
+  end
+  
+  test "Importing a pre-existing product" do
+    skip
+  end
+  
+  test "Importing a new product" do
+    skip
   end
   
 end
