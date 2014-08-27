4db8501d3e8dba812771291dae222c8d10780c3e
Eric Hayes <eric@erichayes.net>
2014-08-27 14:17:07 -0700

    Annotations

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: imports
+#
+#  id           :integer          not null, primary key
+#  catalog_file :string(255)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Import < ActiveRecord::Base
   mount_uploader :catalog_file, FileUploader
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: sites
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  code       :integer
+#  abbr       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Site < ActiveRecord::Base
 end
