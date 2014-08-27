b7d05fa088990af3ce3b15d6e12da5ebe4e95aea
Eric Hayes <eric@erichayes.net>
2014-08-27 14:21:52 -0700

    UI for importing and displaying products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @groups = ProductGroup.includes(:products)
   end
 
   # GET /products/1
