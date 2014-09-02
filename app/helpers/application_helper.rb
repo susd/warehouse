391aee93fb3c54a75052ca65cb3cc4c584390598
Eric Hayes <eric@erichayes.net>
2014-09-02 14:12:17 -0700

    Added links to header; better active_li helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
 module ApplicationHelper
   def active_li(options)
-    cls = "active" if current_page?(options)
-    content_tag :li, class: cls do
+    active = false
+    if options[:action].nil?
+      active = (controller_name == options[:controller])
+    else
+      active = current_page?(options)
+    end
+    html_opts = {}
+    html_opts.merge!(class: "active") if active
+    content_tag :li, html_opts do
       yield if block_given?
     end
   end
