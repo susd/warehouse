aca96fdd773e4b41ce3139146a72194a7b84e25a
Eric Hayes <eric@erichayes.net>
2014-12-08 08:49:19 -0800

    Use PG in dev; upgraded Rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,9 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets, 
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  # config.assets.digest = true
+  config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
