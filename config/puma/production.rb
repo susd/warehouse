98088b66e28b1a664d9b4a35e40829ec28b3dfe4
Eric Hayes <eric@erichayes.net>
2014-12-09 15:45:29 -0800

    Fix to match deploy script

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,7 +1,7 @@
 app = 'warehouse'
 bind "unix:///srv/rails/#{app}/current/tmp/sockets/puma.sock"
-pidfile "/srv/rails/#{app}/current/tmp/puma/pid"
-state_path "/srv/rails/#{app}/current/tmp/puma/state"
+pidfile "/srv/rails/#{app}/current/tmp/pids/puma"
+state_path "/srv/rails/#{app}/current/tmp/pids/puma_state"
 environment 'production'
 rackup "/srv/rails/#{app}/current/config.ru"
 activate_control_app
