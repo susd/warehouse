443fc01f51db9c20f12bdca38196a6543ccd7143
Eric Hayes <eric@erichayes.net>
2015-07-20 15:10:41 -0700

    WIP changing approval

--- app/presenters/order_presenter.rb
+++ app/presenters/order_presenter.rb
@@ -1,40 +1,40 @@
 class OrderPresenter < BasePresenter
   presents :order
-  
+
   def workflow_button
-    if order.submitted?
-      workflow_status_msg
+    if order.editable_by? current_user
+      tpl.link_to workflow_label, workflow_path, class: 'btn btn-primary', method: :put
     else
-      if order.editable_by? current_user
-        tpl.link_to workflow_label, workflow_path, class: css, method: :put
-      else
-        workflow_waiting_msg
-      end
+      workflow_status_msg
     end
   end
-  
+
   def workflow_path
     case order.state
     when 'draft'
       tpl.submit_order_path(order)
+    when 'submitted'
+      tpl.approve_order_path(order)
     when 'approved'
       tpl.fulfill_order_path(order)
     when 'fulfilled'
       tpl.archive_order_path(order)
     end
   end
-  
+
   def workflow_label
     case order.state
     when 'draft'
       'Submit For Approval'
+    when 'submitted'
+      'Approve'
     when 'approved'
       'Mark Order Fulfilled'
     when 'fulfilled'
       'Archive Order'
     end
   end
-  
+
   def workflow_status_msg
     case order.state
     when 'draft'
@@ -47,43 +47,43 @@ class OrderPresenter < BasePresenter
       'Fulfilled'
     end
   end
-  
+
   def approval_button
     tpl.link_to approval_button_path, approval_button_options do
       approval_button_icon
     end
   end
-  
+
   def show_approval_button?
     current_user.quantity_approver?
   end
-  
+
   def approved_by?(role)
     order.approvals.where(role: role).any?
   end
-  
+
   def approval_button_path
     params = {approval: {role_id: quantity_role.id}}
-    
+
     if current_user.roles.include? quantity_role
       path = tpl.order_approvals_path(order, params)
     else
       path = "javascript:;"
     end
-    
+
     path
   end
-  
+
   private
-  
+
   def current_user
     tpl.current_user
   end
-  
+
   def quantity_role
     @role ||= Role.find_by(name: 'quantity')
   end
-  
+
   def approval_button_options
     classes = ['btn']
     if approved_by?(quantity_role)
@@ -91,16 +91,16 @@ class OrderPresenter < BasePresenter
     else
       classes << 'btn-default'
     end
-    
+
     {
       class: classes.join(' '),
       method: :post
     }
   end
-  
+
   def approval_button_icon
     icon = approved_by?(quantity_role) ? 'glyphicon-ok' : 'glyphicon-time'
     tpl.content_tag(:span, nil, id: "approve_for_quantity", class: "glyphicon #{icon}") + " Approve"
   end
-  
+
 end
--- test/integration/workflow_test.rb
+++ test/integration/workflow_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class WorkflowTest < ActionDispatch::IntegrationTest
-  
+
   test "starting a new order" do
     with_user(users(:staff_user)) do
       assert_nothing_raised do
@@ -9,66 +9,49 @@ class WorkflowTest < ActionDispatch::IntegrationTest
       end
     end
   end
-  
+
   test "Viewing orders" do
     with_user(users(:staff_user)) do
       visit orders_path
       assert page.has_css?('.order-row')
     end
   end
-  
+
   test "Approving order" do
-    with_user(users(:principal_user)) do
-      visit order_path(orders(:submitted_order))
-      
-      assert page.has_content?('Principal')
-      assert page.has_content?('Quantity')
-      
-      refute page.has_content?('Send for Review')
-      
-      click_link('Principal')
-      
-      assert_equal order_path(orders(:submitted_order)), current_path
-      assert page.has_content? 'Order approved.'
-    end
-    
     with_user(users(:quantity_user)) do
       visit order_path(orders(:submitted_order))
-      
-      assert page.has_content?('Principal')
-      assert page.has_content?('Quantity')
-      
-      click_link('Quantity')
-      
-      assert_equal order_path(orders(:submitted_order)), current_path
+
+      click_link('Approve')
+
+      assert_equal orders_path, current_path
       assert page.has_content? 'Order approved.'
     end
   end
-  
+
   test "Warehouse list" do
     with_user(users(:warehouse_user)) do
       visit approved_orders_path
-      
+
       assert page.has_css?('.order-row')
     end
   end
-  
+
   test "Fulfilling order" do
     with_user(users(:warehouse_user)) do
       visit order_path(orders(:approved_order))
       click_link('Mark Order Fulfilled')
-      
+
       assert page.has_content? 'Order marked fulfilled.'
     end
   end
-  
+
   test "Archiving order" do
     with_user(users(:finance_user)) do
       visit order_path(orders(:fulfilled_order))
       click_link('Archive Order')
-      
+
       assert page.has_content? 'Order archived.'
     end
   end
-  
+
 end
