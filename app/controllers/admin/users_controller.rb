4dc7ca26d288bf7f8b646acf76f7a75ed945abea
Eric Hayes <eric@erichayes.net>
2015-07-31 09:59:19 -0700

    Set multiple roles from admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,14 +2,14 @@ class Admin::UsersController < AdminController
   before_action :set_user, except: :index
   before_action :set_roles_and_sites, except: :index
   after_action :authorize_for_admin
-  
+
   def index
     @users = User.all
   end
-  
+
   def edit
   end
-  
+
   def update
     if @user.update(user_params)
       redirect_to admin_users_path, notice: 'User updated'
@@ -17,19 +17,19 @@ class Admin::UsersController < AdminController
       render action: :edit
     end
   end
-  
+
   private
-  
+
   def set_user
     @user = User.find params[:id]
   end
-  
+
   def set_roles_and_sites
-    @roles = Role.all.map{|r| [r.name, r.id]}
+    @roles = Role.all
     @sites = Site.all.map{|s| [s.name, s.id]}
   end
-  
+
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :role_ids, :site_id)
+    params.require(:user).permit(:first_name, :last_name, {role_ids: []}, :site_id)
   end
 end
