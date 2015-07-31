b8c2ae4cf9f99fd5181398896bdca00c39a4852b
Eric Hayes <eric@erichayes.net>
2015-07-31 15:06:24 -0700

    Oops: fix problem when not signed-in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def active_li(options)
     controller = options[:controller] || controller_name
     action = options[:action] || action
-    
+
     active = current_resource?(controller, action)
     # active = false
     # if options[:action].nil?
@@ -16,15 +16,24 @@ module ApplicationHelper
       yield if block_given?
     end
   end
-  
+
   def current_resource?(controller, action)
     (controller_name == controller) && (action_name == action)
   end
-  
+
   def present(object, klass = nil)
     klass ||= "#{object.class}Presenter".constantize
     presenter = klass.new(object, self)
     yield presenter if block_given?
     presenter
+  end
+
+  def user_not_configured?
+    return true if current_user.nil?
+    if current_user.admin?
+      false
+    else
+      current_user.roles.empty? && current_user.site.nil?
+    end
   end
 end
